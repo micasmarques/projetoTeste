ab9adf10242f6529bd5031c8ebe393fcc5bf14e0
rayannebarros <40355819+rayannebarros@users.noreply.github.com>
2021-09-15 12:20:42 -0300

    Update Triagem.java

--- src/main/java/br/edu/ifpb/Triagem.java
+++ src/main/java/br/edu/ifpb/Triagem.java
@@ -1,5 +1,41 @@
 package br.edu.ifpb;
 
-public class Triagem {
+public class Triagem extends Doador {
+    public String aptodoarSangue;
+    public String mensagem;
+    public String justificativa;
 
+    public String etapa1() {
+        if (this.getIdade() <= 16 || this.getIdade() >= 69) {
+            mensagem = "Não é possível doar sangue";
+            justificativa = "É necessário ter entre 16 e 69 anos";
+            return mensagem;
+            return justificativa;
+        }
+
+        if (this.getIdade() > 60 && this.isDoaSangue() == False) {
+            mensagem = "Não é possível doar sangue";
+            justificativa = "É necessário ter  doado sangue alguma vez antes";
+            return mensagem;
+            return justificativa;
+        }
+
+        if (this.getSexo() == "Masculino" && this.getPeso() < 60) {
+            mensagem = "Não é possível doar sangue";
+            justificativa = "Pessoas do sexo masculino devem pesar no mínimo 60 kg";
+            return mensagem;
+            return justificativa;
+        }
+
+        if (this.getSexo() == "Feminino" && this.getPeso() < 50) {
+            mensagem = "Não é possível doar sangue";
+            justificativa = "Pessoas do sexo feminino devem pesar no mínimo 50 kg";
+            return mensagem;
+            return justificativa;
+        }
+        mensagem = "Você está apto(a) para a Etapa 2";
+        return mensagem;
+
+    }
 }
+
