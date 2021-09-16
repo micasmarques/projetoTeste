8844c6dca3212daca4d4efd1496befcada45a0ac
Micael Marques <micasmarques1132@gmail.com>
2021-09-15 22:01:21 -0300

    triagem feita

---
+++ src/main/java/br/edu/ifpb/Mensagem.java
@@ -0,0 +1,11 @@
+package br.edu.ifpb;
+
+public class Mensagem {
+    public void reprovado(){
+    System.out.println("Não é possivel doar sangue.");
+    }
+
+    public void aprovado(){
+         System.out.println("Você foi aprovado para doação.");
+    }
+}
--- src/main/java/br/edu/ifpb/Triagem.java
+++ src/main/java/br/edu/ifpb/Triagem.java
@@ -1,41 +1,45 @@
 package br.edu.ifpb;
 
-public class Triagem extends Doador {
-    public String aptodoarSangue;
-    public String mensagem;
-    public String justificativa;
-
-    public String etapa1() {
-        if (this.getIdade() <= 16 || this.getIdade() >= 69) {
-            mensagem = "Não é possível doar sangue";
-            justificativa = "É necessário ter entre 16 e 69 anos";
-            return mensagem;
-            return justificativa;
+public class Triagem {
+    Mensagem m = new Mensagem();
+    public boolean Etapa1(Doador doador) {
+        if ((doador.getPeso() >= 60 && doador.getSexo().equalsIgnoreCase("M")) ||
+                doador.getPeso() >= 50 && doador.getSexo().equalsIgnoreCase("F")) {
+            if (doador.getIdade() >= 16 && doador.getIdade() <= 69) {
+                if (doador.getIdade() >= 60 && !doador.isDoaSangue()) {
+                    m.reprovado();
+                    System.out.println("Doador com idade maior que 60 anos que nunca doou sangue anteriormente!");
+                    return false;
+                } else {
+                    m.aprovado();
+                    return true;
+                }
+            } else {
+                m.reprovado();
+                System.out.println("Doador não está dentro da faixa-etária permitida");
+                return false;
+            }
+        } else {
+            m.reprovado();
+            System.out.println("Doador não está dentro da faixa de peso permitida");
+            return false;
         }
+    }
 
-        if (this.getIdade() > 60 && this.isDoaSangue() == False) {
-            mensagem = "Não é possível doar sangue";
-            justificativa = "É necessário ter  doado sangue alguma vez antes";
-            return mensagem;
-            return justificativa;
-        }
-
-        if (this.getSexo() == "Masculino" && this.getPeso() < 60) {
-            mensagem = "Não é possível doar sangue";
-            justificativa = "Pessoas do sexo masculino devem pesar no mínimo 60 kg";
-            return mensagem;
-            return justificativa;
-        }
-
-        if (this.getSexo() == "Feminino" && this.getPeso() < 50) {
-            mensagem = "Não é possível doar sangue";
-            justificativa = "Pessoas do sexo feminino devem pesar no mínimo 50 kg";
-            return mensagem;
-            return justificativa;
-        }
-        mensagem = "Você está apto(a) para a Etapa 2";
-        return mensagem;
-
+    public boolean Etapa2 (Doador doador) {
+        if (doador.isTemFebre()) {
+            m.reprovado();
+            System.out.println("Doador está com febre");
+            return false;
+        } else if (doador.isGravida()) {
+            m.reprovado();
+            System.out.println("Doador está grávida");
+            return false;
+        } else if (doador.isAmamenta() && doador.isPartoADozeMeses()) {
+            m.reprovado();
+            System.out.println("Parto aconteceu a menos de 12 meses");
+            return false;
+        } System.out.println("Por favor, aguarde na fila de doação!");
+        return true;
     }
 }
-
